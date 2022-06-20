ce811dcc181e04f6cd8b103d6c89be67c14f49d9
Ridvan Kahraman <ridvankahraman@proton.me>
2022-06-20 23:55:05 +0300

    3rd SQL class document was updated.

--- Classes/SQL/SQL_S3_Functions/SQL_S3_Functions.sql
+++ Classes/SQL/SQL_S3_Functions/SQL_S3_Functions.sql
@@ -9,7 +9,6 @@ SELECT product_name FROM product.product
 WHERE SUBSTRING(product_name, 1, CHARINDEX(' ', product_name)) = 'Samsung'
 ORDER BY product_name ASC
 
-
 SELECT COUNT (product_name) FROM product.product WHERE SUBSTRING(product_name, 1, 8) = 'Samsung'
 
 
